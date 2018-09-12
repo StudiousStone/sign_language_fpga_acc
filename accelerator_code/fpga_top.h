f695ba7b005af266ef96b7fb0cd2eea38a606353
pablofsf <ablocorrea@hotmail.com>
2018-09-12 21:50:01 +0200

    Corrected license and copyright notice in files

--- accelerator_code/fpga_top.h
+++ accelerator_code/fpga_top.h
@@ -1,9 +1,17 @@
-/*
- * Copyright (C) 2017 Pablo Correa Gómez
- *
- * GPLv3+
- *
- */
+/************************************************************************************
+* Copyright (C) 2018 Pablo Correa Gomez										        *
+*                                                                                   *
+*    This program is free software; you can redistribute it and/or modify           *
+*    it under the terms of the GNU Affero General Public License as                 *
+*    published by the Free Software Foundation; either version 3 of                 *
+*    the License, or (at your option) any later version.                            *
+*                                                                                   *
+*    This program is distributed in the hope that it will be useful,                *
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of                 *
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                  *
+*    GNU General Public License for more details.                                   *
+*    ( http://www.fsf.org/licenses/agpl.txt )                                       *
+************************************************************************************/
 
 #ifndef __FPGA_TOP_H__
 #define __FPGA_TOP_H__
